cc1cbb0a3ccae235a0003173a681cb31150644f1
Thomas Glasser <thomas@thomasglasser.dev>
2025-03-09 06:28:13 -0400

    Added analytics

--- src/app/Main.tsx
+++ src/app/Main.tsx
@@ -6,6 +6,7 @@ import { App } from './App.js'
 import { LocaleProvider, ProjectProvider, StoreProvider, ThemeProvider, TitleProvider, VersionProvider } from './contexts/index.js'
 import { ModalProvider } from './contexts/Modal.jsx'
 import { SpyglassProvider } from './contexts/Spyglass.jsx'
+import { Analytics } from "@vercel/analytics/react"
 
 function Main() {
 	return (
@@ -18,6 +19,7 @@ function Main() {
 								<ProjectProvider>
 									<ModalProvider>
 										<App />
+										<Analytics />
 									</ModalProvider>
 								</ProjectProvider>
 							</SpyglassProvider>
